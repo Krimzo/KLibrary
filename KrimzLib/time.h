e609006f506402ff065d4dba589b1691c5caac9f
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2021-10-14 09:06:20 +0200

    stopwatch and more time tweaks

--- KrimzLib/time.h
+++ KrimzLib/time.h
@@ -11,6 +11,7 @@ namespace kl
 		time()
 		{
 			QueryPerformanceCounter(&counterLast);
+			QueryPerformanceCounter(&stopwatchLast);
 		}
 
 		// Loads the current pc frequency
@@ -19,25 +20,32 @@ namespace kl
 			LARGE_INTEGER counterTempFreq;
 			QueryPerformanceFrequency(&counterTempFreq);
 			PCFrequency = double(counterTempFreq.QuadPart);
+			StaticGetElapsed();
+			StaticStopwatchReset();
 		}
 
-		// Returns a time since the the last static GetElapsed call
-		static double GetStaticElapsed()
+		// Returns a time since the the last StaticGetElapsed() call
+		static double StaticGetElapsed()
 		{
-			static LARGE_INTEGER counterStaticNow, counterStaticLast;
-			QueryPerformanceCounter(&counterStaticNow);
-			double time = (counterStaticNow.QuadPart - counterStaticLast.QuadPart) / PCFrequency;
-			counterStaticLast = counterStaticNow;
+			static LARGE_INTEGER staticCounterNow;
+			QueryPerformanceCounter(&staticCounterNow);
+			double time = (staticCounterNow.QuadPart - staticCounterLast.QuadPart) / PCFrequency;
+			staticCounterLast = staticCounterNow;
 			return time;
 		}
 
-		// Returns a time since the the last GetElapsed call
-		double GetElapsed()
+		// Resets the static stopwatch to 0 seconds
+		static void StaticStopwatchReset()
 		{
-			QueryPerformanceCounter(&counterNow);
-			double time = (counterNow.QuadPart - counterLast.QuadPart) / PCFrequency;
-			counterLast = counterNow;
-			return time;
+			QueryPerformanceCounter(&staticStopwatchLast);
+		}
+
+		// Returns the passed time since the last static stopwatch reset
+		static double StaticStopwatchElapsed()
+		{
+			static LARGE_INTEGER staticStopwatchNow;
+			QueryPerformanceCounter(&staticStopwatchNow);
+			return (staticStopwatchNow.QuadPart - staticStopwatchLast.QuadPart) / PCFrequency;
 		}
 
 		// Sleeps for the given time
@@ -59,10 +67,38 @@ namespace kl
 			} while (((sleepCounter.QuadPart - sleepCounterStart.QuadPart) / PCFrequency) < time);
 		}
 
+		// Resets the stopwatch to 0 seconds
+		void StopwatchReset()
+		{
+			QueryPerformanceCounter(&stopwatchLast);
+		}
+
+		// Returns the passed time since the last stopwatch reset
+		double StopwatchElapsed()
+		{
+			static LARGE_INTEGER stopwatchNow;
+			QueryPerformanceCounter(&stopwatchNow);
+			return (stopwatchNow.QuadPart - stopwatchLast.QuadPart) / PCFrequency;
+		}
+
+		// Returns a time since the the last GetElapsed() call
+		double GetElapsed()
+		{
+			static LARGE_INTEGER counterNow;
+			QueryPerformanceCounter(&counterNow);
+			double time = (counterNow.QuadPart - counterLast.QuadPart) / PCFrequency;
+			counterLast = counterNow;
+			return time;
+		}
+
 	private:
 		static double PCFrequency;
-		LARGE_INTEGER counterNow = {};
+		static LARGE_INTEGER staticCounterLast;
+		static LARGE_INTEGER staticStopwatchLast;
 		LARGE_INTEGER counterLast = {};
+		LARGE_INTEGER stopwatchLast = {};
 	};
 	double time::PCFrequency = 0;
+	LARGE_INTEGER time::staticCounterLast = {};
+	LARGE_INTEGER time::staticStopwatchLast = {};
 }
