ea83bf7fb2526b337e621324f19ec47b490e800f
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2021-11-01 10:40:07 +0100

    update

--- src/Test.cpp
+++ src/Test.cpp
@@ -5,17 +5,7 @@ int main() {
     kl::InitLib();
 
 
-    kl::array<int> testArray(10);
-
-    testArray.RunOnAll([](int& elem) {
-        elem = kl::random::Int(0, 10);
-    });
-
-    kl::array<int> b = testArray;
-
-    for(int i=0; i<b.Size(); i++) {
-        std::cout << b[i] << std::endl;
-    }
+    printf("testing");
 
 
     printf("\n");
