d1c1777e420bd7e2cbc96f550306243d2b90ea81
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2021-12-22 18:29:41 +0100

    added a horse

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -8,21 +8,24 @@ int main() {
 	/* Skybox */
 	kl::skybox* cloudsBox = nullptr;
 
-	/* Textures */
-	kl::texture* tableTex = nullptr;
-	kl::texture* checkersTex = nullptr;
-	kl::texture* katanaTex = nullptr;
-
 	/* Meshes */
 	kl::mesh* tableMes = nullptr;
 	kl::mesh* pyramidMes = nullptr;
 	kl::mesh* katanaMes = nullptr;
+	kl::mesh* horseMes = nullptr;
+
+	/* Textures */
+	kl::texture* tableTex = nullptr;
+	kl::texture* checkersTex = nullptr;
+	kl::texture* katanaTex = nullptr;
+	kl::texture* horseTex = nullptr;
 
 	/* Objects */
 	kl::gameobject* table = nullptr;
 	kl::gameobject* pyramid = nullptr;
 	kl::gameobject* katanaL = nullptr;
 	kl::gameobject* katanaR = nullptr;
+	kl::gameobject* horse = nullptr;
 
 	/* User start */
 	testEngine.start = [&]() {
@@ -43,57 +46,70 @@ int main() {
 		tableMes = new kl::mesh("res/objects/table.obj");
 		pyramidMes = new kl::mesh("res/objects/pyramid.obj");
 		katanaMes = new kl::mesh("res/objects/katana.obj");
+		horseMes = new kl::mesh("res/objects/horse.obj");
 
 		/* Texture creation */
 		tableTex = new kl::texture("res/textures/table.png");
 		checkersTex = new kl::texture("res/textures/checkers.png");
 		katanaTex = new kl::texture("res/textures/katana.png");
+		horseTex = new kl::texture("res/textures/horse.png");
 
 		/* Object creation */
 		table = testEngine.newObject();
 		pyramid = testEngine.newObject();
 		katanaL = testEngine.newObject();
 		katanaR = testEngine.newObject();
+		horse = testEngine.newObject();
 
 		/* Mesh binding */
 		table->mesh = tableMes;
 		pyramid->mesh = pyramidMes;
 		katanaL->mesh = katanaMes;
 		katanaR->mesh = katanaMes;
+		horse->mesh = horseMes;
 
 		/* Texture binding */
 		table->texture = tableTex;
 		pyramid->texture = checkersTex;
 		katanaL->texture = katanaTex;
 		katanaR->texture = katanaTex;
+		horse->texture = horseTex;
 
 		/* Object properties setup */
 		table->geometry.size = kl::vec3(1, 1, 1);
+		table->geometry.rotation = kl::vec3(0, 0, 0);
 		table->geometry.position = kl::vec3(0, -0.5, 2);
 		table->physics.enabled = true;
 		table->physics.angular.y = 18;
 		table->physics.gravity = 0;
 
 		pyramid->geometry.size = kl::vec3(0.35, 0.35, 0.35);
+		pyramid->geometry.rotation = kl::vec3(0, 0, 0);
 		pyramid->geometry.position = kl::vec3(0, 0.47, 2);
-		pyramid->visible = true;
 		pyramid->physics.enabled = true;
 		pyramid->physics.angular.y = 18;
 		pyramid->physics.gravity = 0;
 
 		katanaL->geometry.size = kl::vec3(2, 2, 2);
-		katanaL->geometry.rotation = kl::vec3(0, 180, -45);
+		katanaL->geometry.rotation = kl::vec3(0, 180, -42);
 		katanaL->geometry.position = kl::vec3(-1, 0, 2);
 		katanaL->physics.enabled = false;
 		katanaL->physics.angular.y = 36;
 		katanaL->physics.gravity = 0;
 		
 		katanaR->geometry.size = kl::vec3(2, 2, 2);
-		katanaR->geometry.rotation = kl::vec3(0, 0, 45);
+		katanaR->geometry.rotation = kl::vec3(0, 0, 42);
 		katanaR->geometry.position = kl::vec3(1, 0, 2);
 		katanaR->physics.enabled = false;
 		katanaR->physics.angular.y = -36;
 		katanaR->physics.gravity = 0;
+
+		horse->geometry.size = kl::vec3(2, 2, 2);
+		horse->geometry.rotation = kl::vec3(0, 0, 0);
+		horse->geometry.position = kl::vec3(0, 0.55, 2);
+		horse->physics.enabled = true;
+		horse->physics.angular.y = 18;
+		horse->physics.gravity = 0;
 	};
 
 	/* User update */
@@ -147,14 +163,20 @@ int main() {
 	/* Engine creation */
 	testEngine.startNew(kl::size(1600, 900));
 
-	/* Cleanup */
+	/* Deleting skybox */
 	delete cloudsBox;
-	delete tableTex;
-	delete checkersTex;
-	delete katanaTex;
+
+	/* Deleting meshes */
 	delete tableMes;
 	delete pyramidMes;
 	delete katanaMes;
+	delete horseMes;
+
+	/* Deleting textures */
+	delete tableTex;
+	delete checkersTex;
+	delete katanaTex;
+	delete horseTex;
 
 	return 0;
 }
