78da4836b87429ab3be70b33ff929ef783dd146c
Crimson17 <46162167+Krimzo@users.noreply.github.com>
2022-08-14 08:44:37 +0200

    3D Mandelbrot set :)

--- Examples/Shaders/Plane.hlsl
+++ Examples/Shaders/Plane.hlsl
@@ -3,21 +3,49 @@ cbuffer VS_CB : register(b0) {
     matrix WMatrix;
     matrix VPMatrix;
     float4 timeData;
+    float2 coords;
+    float2 zoom;
 };
 
 struct VS_OUT {
     float4 screen : SV_Position;
     float3 world : VS_World;
     float3 normal : VS_Normal;
+    float3 color : VS_Color;
 };
+
+float2 ComplexSqr(float2 a) {
+    return float2(a.x * a.x - a.y * a.y, 2.0f * a.x * a.y);
+}
 
 VS_OUT vShader(float3 position : KL_Position) {
     VS_OUT data;
     
-    position.y = sin(position.x + timeData.x);
+    position.y = 1.0f;
+    data.color = 1.0f;
+    
+    float2 uv = position.xz / 5.0f;
+    uv /= zoom.x;
+    uv += coords;
+    
+    float n = 64.0f;
+    float2 num = uv;
+    for (int i = 0; i < n; i++) {
+        if (length(num) > 2.0f) {
+            float iOn = i / n;
+            position.y *= iOn;
+            data.color *= iOn;
+            break;
+        }
+		
+        num = ComplexSqr(num) + uv;
+    }
+    
+    //position.y = sin(position.x + timeData.x) * sin(position.z);
     
     data.world = mul(float4(position, 1), WMatrix).xyz;
     data.screen = mul(float4(data.world, 1), VPMatrix);
+    //data.color = 1.0f;
     
     return data;
 }
@@ -42,7 +70,7 @@ cbuffer PS_CB : register(b0) {
 };
 
 float4 pShader(VS_OUT vsData) : SV_Target{
-    float3 pixel = { 1, 1, 1 };
+    float3 pixel = vsData.color;
     
     vsData.normal = normalize(vsData.normal);
 
