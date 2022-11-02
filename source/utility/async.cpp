59f924ac3ccc33b802cae206918f28dfc2a74db4
Krimzo <46162167+Krimzo@users.noreply.github.com>
2022-11-02 11:33:59 +0100

    Async loop tweak

--- source/utility/async.cpp
+++ source/utility/async.cpp
@@ -8,7 +8,7 @@ void kl::async::loop(const int64_t start_inclusive, const int64_t end_exclusive,
     std::atomic work_counter = start_inclusive;
 
     for (uint32_t t = 0; t < thread_count; t++) {
-        workers[t] = std::thread([&, t] {
+        workers[t] = std::thread([&work_counter, end_exclusive, &loop_body, t] {
             for (int64_t work_index = work_counter++; work_index < end_exclusive; work_index = work_counter++) {
                 loop_body(t, work_index);
             }
