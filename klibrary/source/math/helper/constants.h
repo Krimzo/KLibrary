4e6f4dd01bbf6453c92a6b949b1c27f8580502d3
Ivan P <46162167+Krimzo@users.noreply.github.com>
2023-09-17 14:10:31 +0200

    Update constants.h

--- klibrary/source/math/helper/constants.h
+++ klibrary/source/math/helper/constants.h
@@ -2,11 +2,11 @@
 
 
 namespace kl {
-    inline static constexpr float pi = 3.14159265358979f;
+    inline constexpr float pi = 3.14159265358979f;
 
-    inline static constexpr float to_radians = pi / 180.0f;
-    inline static constexpr float to_degrees = 180.0f / pi;
+    inline constexpr float to_radians = pi / 180.0f;
+    inline constexpr float to_degrees = 180.0f / pi;
 
-    inline static constexpr float to_float_color = 1.0f / 255.0f;
-    inline static constexpr float to_byte_color = 255.0f;
+    inline constexpr float to_float_color = 1.0f / 255.0f;
+    inline constexpr float to_byte_color = 255.0f;
 }
