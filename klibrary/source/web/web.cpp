271d8394b00adfdd4def9289f2c70be3c8ef5d12
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2026-06-04 18:22:42 +0200

    add http request

--- klibrary/source/web/web.cpp
+++ klibrary/source/web/web.cpp
@@ -3,23 +3,36 @@
 
 std::string kl::download_website( std::string_view const& url, int buffer_size )
 {
-    HINTERNET connection = InternetOpenA( "Browser", INTERNET_OPEN_TYPE_PRECONFIG, nullptr, nullptr, 0 );
+    Internet connection = InternetOpenA( "Browser", INTERNET_OPEN_TYPE_PRECONFIG, nullptr, nullptr, 0 );
     if ( !verify( connection, "Failed to open a browser" ) )
         return {};
 
-    HINTERNET address = InternetOpenUrlA( connection, url.data(), nullptr, 0, INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_KEEP_CONNECTION, 0 );
+    Internet address = InternetOpenUrlA( connection, url.data(), nullptr, 0, INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_KEEP_CONNECTION, 0 );
     if ( !verify( address, "Failed to open url \"", url, "\"" ) )
-    {
-        InternetCloseHandle( connection );
         return {};
-    }
 
     std::string data;
     std::vector<byte> buffer( buffer_size );
     for ( DWORD bytes_read = 0; InternetReadFile( address, buffer.data(), (DWORD) buffer.size(), &bytes_read ) && bytes_read > 0;)
         data.insert( data.end(), buffer.begin(), buffer.begin() + bytes_read );
-
-    InternetCloseHandle( address );
-    InternetCloseHandle( connection );
     return data;
+}
+
+int kl::send_http_request( std::string_view const& url, int port, std::string_view const& object, std::string_view const& verb, char const* headers, DWORD headers_size, void const* optional, DWORD optional_size )
+{
+    Internet browser = InternetOpenA( "Browser", INTERNET_OPEN_TYPE_PRECONFIG, nullptr, nullptr, 0 );
+    if ( !browser )
+        return 1;
+
+    Internet internet = InternetConnectA( browser, url.data(), port, nullptr, nullptr, INTERNET_SERVICE_HTTP, 0, 0 );
+    if ( !internet )
+        return 2;
+
+    Internet request = HttpOpenRequestA( internet, verb.data(), object.data(), nullptr, nullptr, nullptr, INTERNET_FLAG_SECURE, 0 );
+    if ( !request )
+        return 3;
+
+    if ( !HttpSendRequestA( request, headers, headers_size, (void*) optional, optional_size ) )
+        return 4;
+    return 0;
 }
--- klibrary/source/web/web.h
+++ klibrary/source/web/web.h
@@ -11,5 +11,28 @@
 
 namespace kl
 {
+struct Internet : NoCopy, NoMove
+{
+    HINTERNET ptr;
+
+    Internet( HINTERNET ptr )
+        : ptr( ptr )
+    {}
+
+    ~Internet() noexcept
+    {
+        InternetCloseHandle( ptr );
+    }
+
+    operator HINTERNET() const noexcept
+    {
+        return ptr;
+    }
+};
+}
+
+namespace kl
+{
 std::string download_website( std::string_view const& url, int buffer_size = 65536 );
+int send_http_request( std::string_view const& url, int port, std::string_view const& object, std::string_view const& verb, char const* headers, DWORD headers_size, void const* optional, DWORD optional_size );
 }
