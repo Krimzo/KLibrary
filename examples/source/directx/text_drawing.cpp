6c24e591b2c8844adaef03b124cee05600213f71
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2026-08-06 19:12:59 +0200

    Update text_drawing.cpp

--- examples/source/directx/text_drawing.cpp
+++ examples/source/directx/text_drawing.cpp
@@ -17,14 +17,17 @@ int examples::text_drawing_main( int argc, char** argv )
     kl::TextFormat format = gpu.create_text_format( L"roboto", DWRITE_FONT_WEIGHT_NORMAL, DWRITE_FONT_STYLE_NORMAL, 25.0f );
     kl::assert( format, "failed to init text format" );
 
-    gpu.text_batch.resize( 20 );
-    for ( int i = 0; i < (int) gpu.text_batch.size(); i++ )
-    {
-        auto& text = gpu.text_batch[i];
-        text.format = format;
-        text.position = kl::to_ndc<float>( { window.size().x * 0.5f, ( i + 1 ) * 50.0f }, window.size() );
-        text.data = kl::convert_string( kl::format( i, "^2 = ", i * i ) );
-    }
+    std::wstringstream message;
+    for ( int i = 0; i < 20; i++ )
+        message << i << "^2 = " << i * i << '\n';
+
+    auto& text = gpu.text_batch.emplace_back();
+    text.format = format;
+    text.position = { -1.0f, 1.0f };
+    text.rect_size = { 2.0f, 2.0f };
+    text.hor_center = true;
+    text.ver_center = true;
+    text.data = message.str();
 
     while ( window.process() )
     {
